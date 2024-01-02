6ad4dbd1d3df05dc4f1dfa9e50949f4f13a376aa
saurabhsingh33 <saurabh95.singh95@gmail.com>
2024-01-02 17:08:38 +0530

    Adding buildspec and pointing to backend

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:8000";
+const host = "http://whisprly-backend-env-1.eba-aamsv92k.us-east-2.elasticbeanstalk.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
